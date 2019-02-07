dd0e5512792ea3ceb580de055cad51e20bc92897
dtodo1paco <dtodo1paco@gmail.com>
2019-02-07 15:34:48 +0100

    Basic express-site

---
+++ session_2/express-site/server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , template = require('pug').compileFile(__dirname + '/templates/homepage.pug')
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = template({ title: 'Home' })
+    res.send(html)
+    res.render('', {});
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
